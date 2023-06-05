b69b0eedc869d4e7ad9ade9a383601cbe5ecfa20
Sujan Bhujel <bhujelsujan64@gmail.com>
2023-06-05 15:01:42 +0545

    Revert "asdas"
    This reverts commit bbd5d30adbead2c61aa3cecebe8ca94e60668c5f.

--- AddressBook.Application/AddressBookService.cs
+++ AddressBook.Application/AddressBookService.cs
@@ -18,6 +18,7 @@ namespace AddressBook.Application
             _context = context;
         }
 
+
         public async Task<List<GetAll>> GetAllAddressBook(string searchString, string sortColumn, string currentFilter, int? pageNumber, int? pageSize)
         {
             var allList = from pd in _context.PersonalDetails
@@ -28,8 +29,7 @@ namespace AddressBook.Application
                           join ph in _context.Phone on pd.Id equals ph.PersonalDetailId
                           select new GetAll()
                           {
-                              //PersonalDetailId = pd.Id,
-                              PersonalDetailId=pd.Id,
+                              PersonalDetailId = pd.Id,
                               FirstName = pd.FirstName,
                               MiddleName = pd.MiddleName,
                               LastName = pd.LastName,
@@ -126,31 +126,19 @@ namespace AddressBook.Application
         {
             using (var trans = _context.Database.BeginTransaction())
             {
-                var pD = new PersonalDetail();
                 try
                 {
-                    if (addDto.ProfilePicture != null)
+                    //Add Personal Detail
+                    PersonalDetail personalDetail = new PersonalDetail()
                     {
-                        var fileName = Guid.NewGuid().ToString() + Path.GetExtension(addDto.ProfilePicture.FileName);
-                        var filePath = Path.Combine(@"C:\Users\Acer 5\source\repos\AddressBookProject\AddressBookProject\wwwroot\uploads\"+fileName);
-                        using (var stream = new FileStream(filePath, FileMode.Create))
-                        {
-                            await addDto.ProfilePicture.CopyToAsync(stream);
-                        }
-                        //addDto.ProfilePicture = "/uploads/" + fileName;
-                        //Add Personal Detail
-                        PersonalDetail personalDetail = new PersonalDetail()
-                        {
-                            FirstName = addDto.FirstName,
-                            MiddleName = addDto.MiddleName,
-                            LastName = addDto.LastName,
-                            ProfilePicture ="/uploads/"+fileName,
-                            Relationship = addDto.Relationship,
-                        };
-                        pD = _context.Add(personalDetail).Entity;
-                        _context.SaveChanges();
-                    }
-                    
+                        FirstName = addDto.FirstName,
+                        MiddleName = addDto.MiddleName,
+                        LastName = addDto.LastName,
+                        ProfilePicture = addDto.ProfilePicture,
+                        Relationship = addDto.Relationship,
+                    };
+                    var pD = _context.Add(personalDetail).Entity;
+                    _context.SaveChanges();
 
                     //Add Company
                     Company company = new Company()
@@ -217,7 +205,7 @@ namespace AddressBook.Application
         }
 
 
-        /*public async Task<UpdateDto> UpdateAddressBook(UpdateDto updateDto)
+        public async Task<UpdateDto> UpdateAddressBook(UpdateDto updateDto)
         {
             using (var trans = _context.Database.BeginTransaction())
             {
@@ -302,7 +290,7 @@ namespace AddressBook.Application
                 }
             }
             return null;
-        }*/
+        }
 
         public async Task<UpdateDto> DetailAddressBook(int id)
         {
@@ -319,7 +307,7 @@ namespace AddressBook.Application
                                       FirstName = pd.FirstName,
                                       MiddleName = pd.MiddleName,
                                       LastName = pd.LastName,
-                                      SProfilePicture = pd.ProfilePicture,
+                                      ProfilePicture = pd.ProfilePicture,
                                       Relationship = pd.Relationship,
                                       Email = e.Email,
                                       PermanentPlaceName = pa.PlaceName,
@@ -338,13 +326,13 @@ namespace AddressBook.Application
                                       PhoneId = ph.Id,
                                       PermanentId = pa.Id,
                                       TemporaryId = ta.Id
-                                  }).FirstOrDefaultAsync();
+                                  }).FirstOrDefaultAsync();            
             abDetail.Relationships = PopulateRelation();
             abDetail.EmailTypes = PopulateEmail();
             abDetail.NumberTypes = PopulateNumber();
-
+            
             abDetail.PermanentProvinces = PopulateProvince();
-
+            
             abDetail.TempProvinces = PopulateProvince();
             return abDetail;
 
--- AddressBookProject/Controllers/AddressBookController.cs
+++ AddressBookProject/Controllers/AddressBookController.cs
@@ -16,9 +16,9 @@ namespace AddressBook.UI.Controllers
             _env = env;
         }
 
-        public async Task<IActionResult> Index(string searchString, string sortOrder, string? currentFilter, int? pageNumber, int? pagesize = 5)
+        public async Task<IActionResult> Index(string searchString, string sortOrder, string? currentFilter, int? pageNumber,int? pagesize=5)
         {
-            var result = await _aBService.GetAllAddressBook(searchString, sortOrder, currentFilter, pageNumber, pagesize);
+            var result = await _aBService.GetAllAddressBook(searchString, sortOrder, currentFilter, pageNumber,pagesize);
             return View(result);
 
         }
@@ -30,14 +30,24 @@ namespace AddressBook.UI.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(AddDto addDto)
+        public async Task<IActionResult> Create(AddDto addDto,IFormFile ProfilePicture)
         {
             if (ModelState.IsValid)
             {
-                await _aBService.AddAddressBook(addDto);
+                if (ProfilePicture != null && ProfilePicture.Length > 0)
+                {
+                    var fileName = Guid.NewGuid().ToString() + Path.GetExtension(ProfilePicture.FileName);
+                    var filePath = Path.Combine(_env.WebRootPath, "uploads", fileName);
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await ProfilePicture.CopyToAsync(stream);
+                    }
+                    addDto.ProfilePicture = "/uploads/" + fileName;
+                }
+                    await _aBService.AddAddressBook(addDto);
                 return RedirectToAction("Index");
             }
-            var data = await _aBService.GetAllEnum();
+            var data=await _aBService.GetAllEnum();
             return View(data);
         }
 
@@ -47,51 +57,51 @@ namespace AddressBook.UI.Controllers
             return View(data);
         }
 
-        /*public async Task<IActionResult> Update(int id)
+        public async Task<IActionResult> Update(int id)
         {
             var data = await _aBService.DetailAddressBook(id);
             return View(data);
-        }*/
+        }
 
-        /* [HttpPost]
-         public async Task<IActionResult> Update(UpdateDto updateDto,IFormFile? ProfilePicture)
-         {
-             var data = await _aBService.DetailAddressBook(updateDto.PersonalDetailId);
-             if (ModelState.IsValid)
-             {
-                 if (ProfilePicture != null && ProfilePicture.Length > 0)
-                 {
-                     var fileName = Guid.NewGuid().ToString() + Path.GetExtension(ProfilePicture.FileName);
-                     var filePath = Path.Combine(_env.WebRootPath, "uploads", fileName);
-                     using (var stream = new FileStream(filePath, FileMode.Create))
-                     {
-                         await ProfilePicture.CopyToAsync(stream);
-                     }
-                     updateDto.ProfilePicture = "/uploads/" + fileName;
-                 }
+        [HttpPost]
+        public async Task<IActionResult> Update(UpdateDto updateDto,IFormFile? ProfilePicture)
+        {
+            var data = await _aBService.DetailAddressBook(updateDto.PersonalDetailId);
+            if (ModelState.IsValid)
+            {
+                if (ProfilePicture != null && ProfilePicture.Length > 0)
+                {
+                    var fileName = Guid.NewGuid().ToString() + Path.GetExtension(ProfilePicture.FileName);
+                    var filePath = Path.Combine(_env.WebRootPath, "uploads", fileName);
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await ProfilePicture.CopyToAsync(stream);
+                    }
+                    updateDto.ProfilePicture = "/uploads/" + fileName;
+                }
 
-                 if (ProfilePicture == null)
-                 {
-                     updateDto.ProfilePicture = data.ProfilePicture;
-                 }
-                 var upd = await _aBService.UpdateAddressBook(updateDto);
-                 return RedirectToAction("Index");
-             }          
-             return View(data);
-         }*/
+                if (ProfilePicture == null)
+                {
+                    updateDto.ProfilePicture = data.ProfilePicture;
+                }
+                var upd = await _aBService.UpdateAddressBook(updateDto);
+                return RedirectToAction("Index");
+            }          
+            return View(data);
+        }
 
-        /* public async Task<IActionResult> Delete(int id)
-         {
-             if (id == null)
-             {
-                 return NotFound();
-             }
-             var data = await _aBService.DetailAddressBook(id);
-             if (data == null)
-             {
-                 return NotFound();
-             }
-             return View(data);
-         }*/
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var data = await _aBService.DetailAddressBook(id);
+            if (data == null)
+            {
+                return NotFound();
+            }
+            return View(data);
+        }
     }
 }
